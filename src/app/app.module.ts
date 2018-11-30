f29de3b1de0bf86eb2f2d1abc2d4dce9285d7e47
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2018-11-30 17:03:57 +0530

    dropdown databainding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxDatatableModule } from '@swimlane/ngx-datatable'
+import  { NgbModule} from '@ng-bootstrap/ng-bootstrap'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDatatableModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
