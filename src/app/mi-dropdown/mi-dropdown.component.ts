84ae34a2a13891e9b8ba8dbdf96f907b2cb78440
MITEL\kattilp <prajeen.kattil@mitel.com>
2019-01-28 14:26:17 +0530

    ng slect with button click

--- src/app/mi-dropdown/mi-dropdown.component.ts
+++ src/app/mi-dropdown/mi-dropdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild,TemplateRef, ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ElementRef } from '@angular/core';
+import { NgSelectComponent } from '@ng-select/ng-select';
 
 @Component({
   selector: 'mi-dropdown',
@@ -8,23 +9,32 @@ import { Component, OnInit, ViewChild,TemplateRef, ElementRef} from '@angular/co
 export class MiDropdownComponent implements OnInit {
   @ViewChild('midropdown1') tpl: ElementRef;
   constructor() { }
-  size:number=0;
-  divVisible:boolean=false;
+  size: number = 0;
+  isControlVisible:boolean = true;
+  isOpen:boolean = false;
+  divVisible: boolean = false;
   status_pair: any[] = [{ id: 1, val: 'Available' }, { id: 2, val: 'Busy' }, { id: 3, val: 'Meeting' }, { id: 4, val: 'Vacation' }];
 
 
   ngOnInit() {
   }
   element: HTMLElement;
-  divclick( sle: HTMLSelectElement) {
-     // this.divVisible= !this.divVisible;
-      // alert('test: click');
-      // console.log("PRajeen");
-      //this.element = document.getElementById('midropdown1') as HTMLElement;
-      this.size=10;
-      this.tpl.nativeElement.click()
-     console.log( this.tpl.nativeElement);
+  divclick(sle: HTMLSelectElement) {
+    // this.divVisible= !this.divVisible;
+    // alert('test: click');
+    // console.log("PRajeen");
+    // this.element = document.getElementById('midropdown1') as HTMLElement;
+    this.size = 10;
+    this.tpl.nativeElement.click()
+    console.log(this.tpl.nativeElement);
 
+  }
+  openSelect(select: NgSelectComponent) {
+    select.open();
+  }
+  private onClick() {
+    // this.isControlVisible = !this.isControlVisible;
+    this.isOpen = true;
   }
 
 
